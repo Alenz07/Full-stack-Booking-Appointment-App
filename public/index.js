37ec1d9f04d9204ec492208a2ee45f5bba03a886
Divesh <138569534+Alenz07@users.noreply.github.com>
2025-10-17 23:57:58 +0530

    Update index.js
    fixed static js

--- public/index.js
+++ public/index.js
@@ -1,22 +1,52 @@
-const db = require("../utils/database");
-const { Sequelize, DataTypes } = require('sequelize');
-const bookings = db.define("bookings",{
-    id:{
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name:{
-        type:DataTypes.STRING
-    },
-    phone:{
-        type:DataTypes.INTEGER
-  
-    },
-    email:{
-        type: DataTypes.STRING
-    }
+window.onload = getInfo;
 
-})
-module.exports  = bookings
+console.log("javascript is working")
+
+ async function sendInfo(event){
+    console.log("sendInfo called!")
+    event.preventDefault();
+    console.log("preventdefautlcalled")
+    obj = {
+    name:document.getElementById("username").value,
+    phone:  document.querySelector("#phone").value,
+    email: document.querySelector("#email").value
+    }
+    console.log("yes its it")
+
+        try {
+            console.log("Making POST request...") // NEW
+              await axios.post(("http://localhost:2000/booking/api/send"),obj)
+              console.log(" data is sent")
+               getInfo()
+              
+              
+        } catch (error) {
+            console.log("ERROR OBJECT:", error) // See full error
+            console.log("ERROR MESSAGE:", error.message) // See message
+            console.log("ERROR RESPONSE:", error.response) // See server response
+            console.log("ERROR STATUS:", error.response?.status) // See status code
+            console.log(error)
+        }
+
+    
+}
+async function getInfo(){
+    try {
+       const response =  await axios.get("http://localhost:2000/booking/booking")
+       const data = (response.data)
+let list = document.getElementById("booklist")
+if(!list){
+    list = document.createElement("ul")
+    list.id = "booklist"
+    document.body.appendChild(list)
+}
+list.innerHTML = data.map(item =>
+     `<li>Name: ${item.name} Phone: ${item.phone} Email: ${item.email}</li>`
+).join("")
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 
